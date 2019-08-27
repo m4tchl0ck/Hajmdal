5ea8370b2ec3c6a57cb5a274951fcf8981fae653
Adrian <m4tchl0ck@outlook.com>
2019-08-27 10:54:00 +0200

    Show result message after information about faild test

--- tests/hajmdal.test.sh
+++ tests/hajmdal.test.sh
@@ -133,12 +133,14 @@ TEST_CASES=( $(compgen -A function | grep test_) )
 
 for test_case in ${TEST_CASES[@]}; do
     start_at=$(now)
-    result=$($test_case)
+    result=$($test_case 2>&1)
     if [ $? -eq 0 ]; then
         write_result "$start_at [PASS] $test_case"
     else
         EXT=1
         write_result "$start_at [FAIL] $test_case"
+        write_result "Result:"
+        write_result "$result"
     fi
 done
 
