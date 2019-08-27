17c6a7742ad6af8aafddbab538a525aebd7032c4
Adrian <m4tchl0ck@outlook.com>
2019-08-27 10:51:55 +0200

    Message when operation is unknow

--- scripts/hajmdal.sh
+++ scripts/hajmdal.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 hw()
 {
     echo "Hello world" >&1
 }
 
-"$@"
+if declare -f "$1" > /dev/null
+then
+  "$@"
+else
+  echo "'$1' is unknown operation name" >&2
+  exit 1
+fi
