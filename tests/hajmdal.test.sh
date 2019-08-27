d9182f40ce14cd17110a7880ce96be065ebae7ef
Adrian <m4tchl0ck@outlook.com>
2019-08-27 10:51:47 +0200

    Add test for script with little test framework

---
+++ tests/hajmdal.test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+TEST_CASES=( )
+
+TEST_CASES[${#TEST_CASES[@]}]=hajmdal_should_write_hello_world
+hajmdal_should_write_hello_world()
+{
+    local result=$($SUT)
+
+    assert_equal "Hello world" "$result"
+}
+
+########################################
+
+assert_equal()
+{
+    expected_value=$1
+    value=$2
+
+    if [ "$value" = "$expected_value" ]; then
+        true
+    else
+        echo "expected: '$expected_value', but was '$value'" >&2
+        false
+    fi
+}
+
+########################################
+
+RESULT=tests-results.txt
+SUT="scripts/hajmdal.sh"
+EXT=0;
+
+now()
+{
+    echo $(date +"%T")
+}
+
+write_result()
+{
+    echo "$1" >&1
+    echo "$1" >> $RESULT
+}
+
+echo "$(now) Tests started" > $RESULT
+
+for test_case in ${TEST_CASES[@]}; do
+    start_at=$(now)
+    result=$($test_case)
+    if [ $? -eq 0 ]; then
+        write_result "$start_at [PASS] $test_case"
+    else
+        EXT=1
+        write_result "$start_at [FAIL] $test_case"
+    fi
+done
+
+echo "$(now) Tests finished" >> $RESULT
+
+exit $EXT
